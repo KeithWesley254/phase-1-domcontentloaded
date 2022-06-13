71d1464b20fd60402e76c98d24d3a13ef6731d3f
Keith <keithwesley187@gmail.com>
2022-06-13 14:52:22 +0300

    commit tree ee32600e631a7b6665ea51eb3028c3501cd0cb87

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+    cbf();
+  });
+function cbf(){
+    const jkslla = document.getElementById("text")
+    jkslla.textContent = "This is really cool!"
+}
